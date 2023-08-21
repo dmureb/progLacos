e4fa987439eb0df2825efb94e80637b9eccc39ce
Daniel Mureb <danielmureb@hotmail.com>
2023-08-21 11:44:26 -0300

    lista 051

---
+++ lista051/questao11.py
@@ -0,0 +1,15 @@
+'''
+Elaborar um programa que apresente o valor de uma potência de uma base qualquer (Variável b) elevada a um
+expoente qualquer (Variável e), ou seja, de be
+. (Sem usar Math.pow();)
+'''
+
+b = int(input("Informe um número para servir de base: "))
+e = int(input("Informe um número para servir de expoente: "))
+cont = 1
+potencia = b
+
+while (cont < e):
+    potencia = potencia * b
+    cont += 1
+print(f"O resultado da potência é igual a {potencia}")
